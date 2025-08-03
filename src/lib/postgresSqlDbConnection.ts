d15a5ab164efbe1e49fa8662c7ecdc5b33ee2df2
Engr-M-Bilal-Khalid <muhammadbilal41266@gmail.com>
2025-08-03 23:47:48 +0500

    Update postgresSqlDbConnection.ts
    Update connection

--- src/lib/postgresSqlDbConnection.ts
+++ src/lib/postgresSqlDbConnection.ts
@@ -2,123 +2,16 @@ import { Pool } from 'pg';
 
 let pool: Pool | null = null;
 
-// export default function Database() {
-//     if (!pool) {
-//         pool = new Pool({ 
-//             host: 'localhost',
-//             port: 5432,
-//             user: 'postgres',
-//             password: 'bilalkhalid',
-//             database: 'alibaba_ecommerce',
-//         });
-//         console.log("✅ PostgreSQL pool created");
-//     }
-//     return pool;
-// };
-
-
-
-
 export default function Database() {
     if (!pool) {
         pool = new Pool({ 
-            host: 'aws-0-us-east-2.pooler.supabase.com',
+            host: process.env.DB_HOST,
             port: 6543,
-            user: 'postgres.pzuodyywfwkdmxkuynjg',
-            password: 'P9EMRWsz4VtcDCzq',
-            database: 'postgres', // The default database for Supabase is 'postgres'
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DATABASE, // The default database for Supabase is 'postgres'
         });
         console.log("✅ PostgreSQL pool created");
     }
     return pool;
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// host:
-// aws-0-us-east-2.pooler.supabase.com
-
-// port:
-// 6543
-
-// database:
-// postgres
-
-// user:
-// postgres.pzuodyywfwkdmxkuynjg
-
-// pool_mode:
-// transaction
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export default function Database() {
-//     if (!pool) {
-//         pool = new Pool({ 
-//             host: 'db.pzuodyywfwkdmxkuynjg.supabase.co',
-//             port: 6543,
-//             user: 'postgres',
-//             password: 'P9EMRWsz4VtcDCzq',
-//             database: 'postgres', // The default database for Supabase is 'postgres'
-//         });
-//         console.log("✅ PostgreSQL pool created");
-//     }
-//     return pool;
-// };
